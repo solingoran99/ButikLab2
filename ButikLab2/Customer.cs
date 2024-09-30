f69893a9466a62dcddcb7b7f0eecd73a7100386b
solingoran99 <solin.goran.yusuf.rasid@iths.se>
2024-09-30 12:28:50 +0200

    changes

---
+++ ButikLab2/Customer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButikLab2
+{
+	public class Customer
+	{
+		private string _name;
+		private string _Password;
+
+		public Customer(string name, string password)
+		{
+			Name = name;
+			Password = password;
+		}
+
+		public string Name { get => _name; set
+			{
+				if (string.IsNullOrEmpty(value))
+				{
+					throw new InvalidOperationException("Customer name cannot be empty.");
+				}
+				_name = value;
+			}
+		}
+		public string Password { get => _Password; set
+			{
+				if (string.IsNullOrEmpty(value))
+				{
+					throw new InvalidOperationException("Password cannot be empty");
+				}
+				_Password = value;
+			}
+		}
+
+		// Customer login method
+		public static Customer Login(List<Customer> customers)
+		{
+            Console.WriteLine("Enter your username:");
+			string userName = Console.ReadLine();
+
+            Console.WriteLine("Enter your password:");
+			string userPassword = Console.ReadLine();
+
+			Customer loggedInCustomer = customers.Find(c => c.Name == userName && c.Password == userPassword);
+
+			if (loggedInCustomer != null)
+			{
+                Console.WriteLine($"Welcome back, {loggedInCustomer.Name}");
+				return loggedInCustomer;
+            }
+			else
+			{
+                Console.WriteLine("Invalid username or password. Please try again.");
+				return null;
+            }
+        }
+
+		public static void registerCustomer(List<Customer> customers)
+		{
+			Console.WriteLine("To become a member create an account.\nEnter your username:");
+			string userName = Console.ReadLine();
+
+            Console.WriteLine("Enter your password:");
+			string userPassword = Console.ReadLine();
+
+			customers.Add(new Customer(userName, userPassword));
+            Console.WriteLine("Successfully registered!");
+        }
+		
+	  
+		
+	}
+}
--- ButikLab2/Program.cs
+++ ButikLab2/Program.cs
@@ -1,9 +1,73 @@
-﻿namespace ButikLab2
+﻿using System.Net.Http.Headers;
+
+namespace ButikLab2
 {
 	internal class Program
 	{
 		static void Main(string[] args)
-		{
-            Console.WriteLine("Welcome to Solin's Make Up Butik! ");
-        }
+		{   // list of customers
+			List<Customer> customers = new List<Customer>
+			{
+				new Customer("Knatte", "123"),
+				new Customer("Fnatte", "321"),
+				new Customer("Tjatte", "213")
+			};
+
+			
+
+			bool running = true;
+			while (running)
+			{
+				Console.Clear();
+				Console.ForegroundColor = ConsoleColor.Magenta;
+			    Console.WriteLine("Welcome to Lush Locks! To see our newest releases, hurry up and join!");
+			    Console.ResetColor();
+
+				Console.WriteLine("1.Log in\n2.Become a member\n3.Exit\nEnter the corresponding number:");
+				string userInput = Console.ReadLine();
+				int userChoice;
+				
+
+				if (int.TryParse(userInput, out userChoice))
+				{    // handle user choice
+					switch (userChoice)
+					{
+						case 1:
+							Console.Clear();
+							Console.WriteLine("Please log in:");
+							//call login method
+							Customer loggedInCustomer = Customer.Login(customers);
+							Console.WriteLine("Press enter to go back to the menu");
+							Console.ReadKey();
+							break;
+						case 2:
+							Console.Clear();
+							//Call registeration method
+							Customer.registerCustomer(customers);
+							Console.WriteLine("Press enter to go back to the menu");
+							Console.ReadKey();
+							break;
+						case 3:
+							Console.WriteLine("Thank you for your visit!");
+							//set to false and break the loop
+							running = false;
+							break;
+						default:
+							Console.WriteLine("Invalid choice. Choose 1, 2 or 3.");
+							break;
+
+					}
+				}
+				else
+				{
+					Console.WriteLine("OOPS error! Please enter a number.");
+				}
+
+		
+
+			}
+			
+		}
+    } 
 }
+
